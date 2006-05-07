b7c2a380f5193525a0fbd8966a7b12eb950ed1ba
alih <alih@23f6b8fd-2e13-0410-b56e-ca9b1a140600>
2006-05-07 13:42:59 +0000

    first upload

---
+++ trunk/src/thinning.h
@@ -0,0 +1,77 @@
+/* Plasma OCR - an OCR engine
+ *
+ * thinning.h - getting a "framework" of a letter by thinning it.
+ *
+ * Copyright (C) 2006  Ilya Mezhirov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+/* Here's a picture illustrating what is a framework
+ * (view with monospace font):
+ *
+ *  Original letter:         Its framework:
+ *
+  .....@@@@@@@@........  .....................
+  ...@@@@@@@@@@@@......  ......@@@@@..........
+  ..@@@@@@@@@@@@@@.....  .....@@...@@@........
+  ..@@@@@...@@@@@@@....  ....@@......@@.......
+  ..@@@@.....@@@@@@....  ....@........@.......
+  .@@@@@.....@@@@@@....  ....@........@.......
+  .@@@@@.....@@@@@@....  ....@........@.......
+  ..@@@@.....@@@@@@....  ....@........@.......
+  ..........@@@@@@@....  .............@.......
+  .......@@@@@@@@@@....  .............@.......
+  .....@@@@@@@@@@@@....  ........@@@@@@.......
+  ...@@@@@@@@@@@@@@....  ......@@@....@.......
+  ..@@@@@@@..@@@@@@....  .....@@......@.......
+  .@@@@@@....@@@@@@....  ...@@@.......@.......
+  .@@@@@.....@@@@@@....  ...@.........@.......
+  @@@@@......@@@@@@....  ..@@.........@.......
+  @@@@@......@@@@@@....  ..@..........@.......
+  @@@@@.....@@@@@@@....  ..@..........@.......
+  @@@@@@....@@@@@@@.@@@  ..@@.........@.......
+  .@@@@@@@@@@@@@@@@@@@@  ...@@.....@@@@@......
+  .@@@@@@@@@@@@@@@@@@@@  ....@@..@@@...@@@@@..
+  ..@@@@@@@@@.@@@@@@@@.  .....@@@@............
+  ....@@@@@....@@@@@...  .....................
+
+
+*/
+
+
+#ifndef PLASMA_OCR_THINNING_H
+#define PLASMA_OCR_THINNING_H
+
+
+/* Get a framework of the letter.
+ * Returns a bitmap with the same width and height as the source.
+ * The returned result must be disposed with free_bitmap_with_margins().
+ * Might return NULL if malloc() failed.
+ *
+ * Input:  0 - white, nonzero - black
+ * Output: 0 - white, 1 - black
+ * 
+ * WARNING: the original bitmap is filled with garbage.
+ *
+ * Takes time O(width * height * thickness), whatever thickness means.
+ * Just don't use it on the whole page, use on letters instead.
+ */
+unsigned char **thin(unsigned char **pixels, int width, int height,
+                     int use_8_connectivity /* nonzero - true */);
+
+
+#endif
