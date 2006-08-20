ccd3bf561cc508d2e0cb6ab52730ecee54933aff
alih <alih@23f6b8fd-2e13-0410-b56e-ca9b1a140600>
2006-08-20 06:08:28 +0000

    Merged thickening onto thinning.

--- trunk/octave/thin.cpp
+++ trunk/octave/thin.cpp
@@ -15,7 +15,17 @@
 
 
 
-DEFUN_DLD (thin, args, , "thin(A, N=1)\nThin a bitmap N times.")
+DEFUN_DLD (thin, args, ,
+"thin(A, N=1)\n"
+"Morphological thinning/thickening/skeletonization.\n\n"
+"The image should be an 2D uint8 array; the result is also uint8.\n\n"
+"N > 0: thin a bitmap N times.\n"
+"N = 0: skeletonize (thin until it stops changing).\n"
+"N < 0: thicken a bitmap -N times.\n\n"
+"Thinning does not change the size of the image.\n"
+"Thickening enlarges the image by 1 pixel at each side,\n"
+"thus increasing both dimensions by 2.\n"
+)
 {
     unsigned char **pixels;
     int w, h;
@@ -27,8 +37,25 @@ DEFUN_DLD (thin, args, , "thin(A, N=1)\nThin a bitmap N times.")
     if (args.length() > 1)
         N = args(1).int_value();
 
-    unsigned char **output = thin(pixels, w, h, N);
-    octave_value result = bitmap_to_octave(output, w, h);
+    unsigned char **output;
+    octave_value result;
+    
+    if (N > 0)
+    {
+        output = thin(pixels, w, h, N);
+        result = bitmap_to_octave(output, w, h);
+    }
+    else if (N == 0)
+    {
+        output = skeletonize(pixels, w, h, /* use_8_connectivity: */ 0);
+        result = bitmap_to_octave(output, w, h);        
+    }
+    else // N < 0
+    {
+        output = thicken(pixels, w, h, -N);
+        result = bitmap_to_octave(output, w - N * 2, h - N * 2);
+    }
+    
     free_bitmap_with_margins(output);
     free_bitmap(pixels);
     return result;
